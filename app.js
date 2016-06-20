f3125738bd17841a6d7a61c8b66ff502f77d77ba
sherif <sherif.amgad.nabih@gmail.com>
2016-06-20 12:25:12 +0200

    Add doctor model

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-var express= require('express');
-var http= require('http');
-var app=express();
+var express = require('express');
+var http = require('http');
+var _ = require('lodash');
+var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Doctors');
+var Doctor = require('./Models/Doctor.js')
 
-app.get('/',function (request,response) {
-    
+app.get('/', function (request, response) {
+
     response.write('Hello World');
     response.end();
 });
 
-app.listen(8080,function () {
+app.post('/AddDoctor', function (request, response) {
+
+    var newdoctor = Doctor(request.body);
+    newdoctor.save(function (err) {
+        if (err)
+            response.write('failed to add doctor');
+        else
+            response.write('Doctor Added');
+
+        response.end();
+
+    })
+
+
+});
+
+app.listen(8080, function () {
     console.log('server is runnig');
 })

bb51be1061cbd00dbd59448e99c4676a98a02c43
sherif <sherif.amgad.nabih@gmail.com>
2016-06-21 14:00:47 +0200

    Add eslint

--- Models/Doctor.js
+++ Models/Doctor.js
@@ -1,10 +1,10 @@
-var mongoose=require('mongoose');
+var mongoose = require('mongoose')
 
-var DoctorSchema=mongoose.Schema({
-Name:String,
-Phone:String,
-Address:String
+var DoctorSchema = mongoose.Schema({
+  name: String,
+  phone: String,
+  address: String
 
 })
 
-module.exports=mongoose.model('Doctor',DoctorSchema);
+module.exports = mongoose.model('Doctor', DoctorSchema)
--- app.js
+++ app.js
@@ -1,33 +1,51 @@
-var express = require('express');
-var http = require('http');
-var _ = require('lodash');
-var app = express();
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/Doctors');
+var express = require('express')
+var app = express()
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+mongoose.connect('mongodb://localhost/Doctors')
 var Doctor = require('./Models/Doctor.js')
 
 app.get('/', function (request, response) {
-
-    response.write('Hello World');
-    response.end();
-});
+  response.write('Hello World')
+  response.end()
+})
 
 app.post('/AddDoctor', function (request, response) {
+  var newdoctor = Doctor(request.body)
+  newdoctor.save(function (err) {
+    if (err) {
+      response.write('failed to add doctor')
+    } else {
+      response.write('Doctor Added')
+    }
 
-    var newdoctor = Doctor(request.body);
-    newdoctor.save(function (err) {
-        if (err)
-            response.write('failed to add doctor');
-        else
-            response.write('Doctor Added');
+    response.end()
+  })
+})
 
-        response.end();
+app.get('/Doctors', function (request, response) {
+  Doctor.find(function (err, doctors) {
+    if (err) {
+      response.json({info: 'error during find Doctors', error: err})
+    };
+    response.json({info: 'Doctors found successfully', data: doctors})
+  })
+})
 
-    })
-
-
-});
+app.get('/Doctors/:Id', function (request, response) {
+  Doctor.findById(request.params.Id, function (err, doctor) {
+    if (err) {
+      response.json({info: 'error during find doctor', error: err})
+    };
+    response.json({info: 'doctor found successfully', data: doctor})
+  })
+})
 
 app.listen(8080, function () {
-    console.log('server is runnig');
+  console.log('server is runnig')
 })
